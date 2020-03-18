4f7c107777b1cd2ecc93a74218a202778120efed
Jung Hong <junghong0512@gmail.com>
2020-03-18 21:25:28 +0900

    Wrapping a REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,66 @@
-let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The New One",
-    score: 82
-  },
-  {
-    id: 1,
-    name: "Avengers - The New One",
-    score: 89
-  },
-  {
-    id: 2,
-    name: "The GodFather",
-    score: 92
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 79
+// let movies = [
+//   {
+//     id: 0,
+//     name: "Star Wars - The New One",
+//     score: 82
+//   },
+//   {
+//     id: 1,
+//     name: "Avengers - The New One",
+//     score: 89
+//   },
+//   {
+//     id: 2,
+//     name: "The GodFather",
+//     score: 92
+//   },
+//   {
+//     id: 3,
+//     name: "Logan",
+//     score: 79
+//   }
+// ];
+
+// export const getMovies = () => movies;
+
+// export const getById = id => {
+//   const filteredMovies = movies.filter(movie => movie.id === id);
+//   return filteredMovies[0];
+// };
+
+// export const deleteMovie = id => {
+//   const cleanedMovies = movies.filter(movie => movie.id !== id);
+//   if (movies.length > cleanedMovies.length) {
+//     movies = cleanedMovies;
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// export const addMovie = (name, score) => {
+//   const newMovie = {
+//     id: movies.length,
+//     name,
+//     score
+//   };
+//   movies.push(newMovie);
+//   return newMovie;
+// };
+
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === id);
-  return filteredMovies[0];
-};
-
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
   }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
+  return fetch(`${REQUEST_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 };
