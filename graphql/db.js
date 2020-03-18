bded10ac35655d086f4469537532d7c7bd04e844
Jung Hong <junghong0512@gmail.com>
2020-03-18 20:31:00 +0900

    Defining Mutation, Creating and Delete Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,43 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: "0",
-    name: "Hong",
-    age: 30,
-    gender: "male"
+    id: 0,
+    name: "Star Wars - The New One",
+    score: 82
   },
   {
-    id: "1",
-    name: "Jung",
-    age: 31,
-    gender: "male"
+    id: 1,
+    name: "Avengers - The New One",
+    score: 89
   },
   {
-    id: "2",
-    name: "Lee",
-    age: 33,
-    gender: "female"
+    id: 2,
+    name: "The GodFather",
+    score: 92
   },
   {
-    id: "3",
-    name: "Yeun",
-    age: 20,
-    gender: "female"
-  },
-  {
-    id: "4",
-    name: "JH",
-    age: 40,
-    gender: "male"
-  },
-  {
-    id: "5",
-    name: "YLee",
-    age: 29,
-    gender: "female"
+    id: 3,
+    name: "Logan",
+    score: 79
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
